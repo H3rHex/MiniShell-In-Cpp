77e789a81b16ff9ec317b658c34272df387436e4
H3rHex <149667352+H3rHex@users.noreply.github.com>
2025-07-17 01:29:35 +0200

    feat(minishell): Core Command Implementation with Hashing & Chaining
    This commit introduces the foundational command handling for the MiniShell.
    
    Features:
    - **Implemented essential commands:** `help`, `clear`, and `exit`.
    - **Introduced a custom Hash Table:** Manages command lookup efficiently.
    - **Collision resolution via Linked Lists:** Ensures robustness for command collisions.
    - **Developed low-level string utilities:** Custom functions for string manipulation (e.g., `getStringLength`, `compareStrings`, `copyString`, `cleanJumpCharacter`, `toLower`).
    
    Technical Details:
    - Memory management (allocation and deallocation) is handled at a low level using `new` and `delete` operators.
    - Direct system calls (`read`, `write`) are used for I/O operations.
    - ANSI escape codes are utilized for colored prompt and error messages, and screen clearing.

---
+++ out/CMakeFiles/miniShell.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/miniShell.dir/src/includes/lib/strings/strings.cpp.o"
+  "CMakeFiles/miniShell.dir/src/includes/lib/strings/strings.cpp.o.d"
+  "CMakeFiles/miniShell.dir/src/includes/shell/commands/commands.cpp.o"
+  "CMakeFiles/miniShell.dir/src/includes/shell/commands/commands.cpp.o.d"
+  "CMakeFiles/miniShell.dir/src/main.cpp.o"
+  "CMakeFiles/miniShell.dir/src/main.cpp.o.d"
+  "miniShell"
+  "miniShell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/miniShell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
